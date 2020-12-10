6dee4a88c0754941802164832f77aad9424632b3
Filomaster <fpm1504@gmail.com>
2020-12-10 14:09:19 +0100

    Initial commit

---
+++ app/admin.js
@@ -0,0 +1,146 @@
+//#region Page template
+let renderPage = (content, link_id) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <title>Admin</title>
+    </head>
+
+    <body>
+        <div id="navbar" class="navbar">
+            <a href="/main">Main</a>
+            <a class="active" href="/admin">Admin</a>
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+            <a href="/logout">Logout</a>
+        </div>
+        <div class="adminNav">
+            <a ${link_id == 0 ? `class="active"` : ""}href="/show">show</a>
+            <a ${link_id == 1 ? `class="active"` : ""}href="/gender">gender</a>
+            <a ${link_id == 2 ? `class="active"` : ""}href="/sort">sort</a>
+        </div>
+        <div id="content">
+            <div class="formContainer">${content}</div>
+        </div>
+    </body>
+
+    </html>
+`;
+};
+
+//#endregion
+let users = [
+  {
+    login: "admin",
+    password: "admin",
+    age: 30,
+    student: "no",
+    gender: "m",
+    id: 1,
+  },
+  {
+    login: "AAA",
+    password: "AAA",
+    age: 11,
+    student: "yes",
+    gender: "k",
+    id: 2,
+  },
+  { login: "BBB", password: "BBB", age: 14, student: "no", gender: "m", id: 3 },
+  {
+    login: "CCC",
+    password: "CCC",
+    age: 15,
+    student: "yes",
+    gender: "k",
+    id: 4,
+  },
+  {
+    login: "aaa",
+    password: "aaa",
+    age: 10,
+    student: "yes",
+    gender: "m",
+    id: 5,
+  },
+];
+
+let logged = false;
+// null;
+
+module.exports.validateLogin = (login) => {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].login == login.login) {
+      if (users[i].password == login.password) {
+        logged = true;
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+module.exports.getLogin = () => {
+  return logged;
+};
+module.exports.setLogin = (isLogged) => {
+  logged = isLogged;
+};
+
+module.exports.getNextId = () => {
+  return users[users.length - 1].id + 1;
+};
+
+module.exports.getShow = () => {
+  let content = "<table>";
+  users.forEach((user) => {
+    content += `<tr class="${user.id % 2 == 0 ? "even" : "odd"}">
+                    <td style="width: 3em" >id:&Tab;${user.id} </td>
+                    <td style="width: 10em">user:&Tab;${user.login}&Tab;-&Tab;${
+      user.password
+    }</td>
+                    <td style="width: 3em"><label for="student">Uczeń: </label><input type="checkbox" ${
+                      user.student == "yes" ? "checked" : ""
+                    } onclick="return false;" id="student"/></td>
+                    <td style="width: 4em">wiek:&Tab;${user.age}</td>
+                    <td style="width: 4em">płeć:&Tab;${user.gender}</td>
+                </tr>`;
+  });
+  return renderPage(content + "</table>", 0);
+};
+module.exports.getSort = () => {
+  let content = "<table>";
+  users
+    .sort((a, b) => {
+      return a.age - b.age;
+    })
+    .forEach((user) => {
+      content += `<tr class="${user.id % 2 == 0 ? "even" : "odd"}">
+                    <td style="width: 3em" >id:&Tab;${user.id} </td>
+                    <td style="width: 10em">user:&Tab;${user.login}&Tab;-&Tab;${
+        user.password
+      }</td>
+                    <td style="width: 4em">wiek:&Tab;${user.age}</td>
+                </tr>`;
+    });
+  return renderPage(content + "</table>", 2);
+};
+module.exports.getGender = () => {
+  let content = "<table>";
+  users.forEach((user) => {
+    content += `<tr class="${user.id % 2 == 0 ? "even" : "odd"}">
+                    <td style="width: 3em" >id:&Tab;${
+                      user.id
+                    } </td>                    
+                    <td style="width: 4em">płeć:&Tab;${user.gender}</td>
+                </tr>`;
+  });
+  return renderPage(content + "</table>", 1);
+};
+
+module.exports.users = users;
---
+++ app/router.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const router = express.Router();
+const adminPanel = require("./admin");
+
+router.use(express.static(path.join(__dirname, "../static")));
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get("/", (req, res) => {
+  res.redirect("/main");
+});
+
+router.get("/main", (req, res) => {
+  res.sendFile(path.join(__dirname, "../static/pages/main.html"));
+});
+
+router.get("/admin", (req, res) => {
+  if (adminPanel.getLogin())
+    res.sendFile(path.join(__dirname, "../static/pages/admin.html"));
+  else res.sendFile(path.join(__dirname, "../static/pages/admin_denied.html"));
+  //   res.sendFile(path.join(__dirname, "../static/pages/admin.html"));
+});
+
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../static/pages/login.html"));
+});
+router.post("/login", (req, res) => {
+  if (adminPanel.validateLogin(req.body)) res.redirect("/admin");
+  else res.redirect("login");
+});
+
+router.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "../static/pages/register.html"));
+});
+
+router.post("/register", (req, res) => {
+  if (!req.body.hasOwnProperty("student")) req.body.student = "no";
+  req.body.id = adminPanel.getNextId();
+  adminPanel.users.push(req.body);
+  res.redirect("/login");
+  console.log(adminPanel.users);
+});
+
+router.get("/show", (req, res) => {
+  if (adminPanel.getLogin()) res.send(adminPanel.getShow());
+  else res.sendFile(path.join(__dirname, "../static/pages/admin_denied.html"));
+});
+router.get("/gender", (req, res) => {
+  if (adminPanel.getLogin()) res.send(adminPanel.getGender());
+  else res.sendFile(path.join(__dirname, "../static/pages/admin_denied.html"));
+});
+router.get("/sort", (req, res) => {
+  if (adminPanel.getLogin()) res.send(adminPanel.getSort());
+  else res.sendFile(path.join(__dirname, "../static/pages/admin_denied.html"));
+});
+
+router.get("/logout", (req, res) => {
+  adminPanel.setLogin(false);
+  res.redirect("/login");
+});
+module.exports = router;
